b752b13653c07e2c6aac856c5e55fc83e07c31be
Ana Sousa <ana.sousa@innowave.tech>
2023-06-01 23:24:04 +0100

    create sp

---
+++ V002__create_sp_demo.sql
@@ -0,0 +1,12 @@
+CREATE function getCompanyDetailsbyCustomerID (pid integer)
+    returns TABLE(name varchar(50),email varchar(50),phone varchar(10))
+    language plpgsql
+as
+$$
+BEGIN
+    SELECT cp.name, cp.email
+    from Companies cp
+    join Customers ct on cp.id = ct.company
+    where ct.id = pid
+END
+$$;
