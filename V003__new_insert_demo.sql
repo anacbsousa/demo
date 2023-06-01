20b9d99ebc88fd20f9b68f278d740b807bf221be
Ana Sousa <ana.sousa@innowave.tech>
2023-06-01 23:52:46 +0100

    new files

---
+++ V003__new_insert_demo.sql
@@ -0,0 +1,4 @@
+INSERT INTO Customers (id,name,address,email,phone,company)
+VALUES (4,'Donna N.','Street D', 'd@gmail.com','444444',7);
+
+
--- V004__alter_sp_demo.sql
+++ V004__alter_sp_demo.sql
@@ -1,12 +1,14 @@
-CREATE function getCompanyDetailsbyCustomerID (pid integer)
+drop function getCompanyDetailsbyCustomerID;
+
+CREATE or replace function getCompanyDetailsbyCustomerID (pid integer)
     returns TABLE(name varchar(50),email varchar(50),phone varchar(10))
     language plpgsql
 as
 $$
 BEGIN
 	RETURN QUERY
-	
-    (SELECT cp.name, cp.email
+
+    (SELECT cp.name, cp.email, cp.phone
     from Companies cp
     join Customers ct on cp.id = ct.company
     where ct.id = pid);
