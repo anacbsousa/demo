a469cb8308ac3d62b1567923a59bb926dde740f0
Ana Sousa <ana.sousa@innowave.tech>
2023-06-01 23:23:32 +0100

    create and populate tables

--- V001__create_table_demo.sql
+++ V001__create_table_demo.sql
@@ -1,4 +1,3 @@
--- create a table Companies with name, id, address, email, and phone number
 CREATE TABLE Companies (
   id int,
   name varchar(50),
@@ -6,3 +5,22 @@ CREATE TABLE Companies (
   email varchar(50),
   phone varchar(10)
 );
+
+CREATE TABLE Customers (
+  id int,
+  name varchar(50),
+  address text,
+  email varchar(50),
+  phone varchar(10),
+  company int
+);
+
+INSERT INTO Customers (id,name,address,email,phone,company)
+VALUES (1,'John S.','Street A', 'a@gmail.com','111111',13),
+       (2,'Mary L.','Street B', 'b@gmail.com','222222',13),
+       (3,'Robert M.','Street C', 'c@gmail.com','333333',22);
+
+INSERT INTO Companies (id,name,address,email,phone)
+VALUES (7,'Company A','Street X', 'x@gmail.com','121212'),
+       (13,'Company B','Street Y', 'y@gmail.com','232323'),
+       (22,'Company C','Street Z', 'z@gmail.com','454545');
